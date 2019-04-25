f9c7a851b0ea6f6463580e025173341d3b0cfd5b
Krikod <kristen.kod@gmail.com>
2019-04-25 11:56:12 +0200

    QueryBuilder

--- src/AppBundle/Repository/CategoryRepository.php
+++ src/AppBundle/Repository/CategoryRepository.php
@@ -12,10 +12,15 @@ class CategoryRepository extends \Doctrine\ORM\EntityRepository
 {
     public function findAllOrdered()
     {
-        $dql = 'SELECT cat FROM AppBundle\Entity\Category cat ORDER BY cat.name DESC';
-        $query = $this->getEntityManager()->createQuery($dql);
+//        $dql = 'SELECT cat FROM AppBundle\Entity\Category cat ORDER BY cat.name DESC';
+//        $query = $this->getEntityManager()->createQuery($dql);
 //        var_dump($query->getSQL());die;
-
+//        return $query->execute();
+        $qb = $this->createQueryBuilder('cat')
+        ->addOrderBy('cat.name', 'DESC')
+        ;
+        $query = $qb->getQuery();
+//        var_dump($query->getDQL());die;
         return $query->execute();
     }
 }
